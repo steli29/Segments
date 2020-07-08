4e91344e36bc6b704ff2f55f5949321d269957da
Стилян Саравски <steli.saravski@abv.bg>
2020-07-08 14:59:31 +0300

    Add project files.

---
+++ Segments/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Segments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string[] split = line.Split(new char[] { ' ' });
+            int n = int.Parse(split[0]);
+            int a = int.Parse(split[1]);
+            int b = int.Parse(split[2]);
+            int c = int.Parse(split[3]);
+            char[] segment = new char[n + 1];
+            for (int i = 0; i < segment.Length; i+=a)
+            {
+                segment[i] = '#';
+            }
+            for(int j = segment.Length - 1; j >= 0; j-=b)
+            {
+                segment[j] = '#';
+            }
+            for(int k = 0; k < segment.Length; k++)
+            {
+                if (segment[k] != '#')
+                {
+                    segment[k] = '0';
+                }
+            }
+
+            int counter = 0;
+            for (int l = 0; l < segment.Length; l++)
+            {
+                if(l+c < segment.Length)
+                {
+                    if (segment[l] == segment[l + c]) continue;
+                    else counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
